47807a479919f0ce8ab9a4107092a1234198b8dd
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-25 10:37:54 +0900

    .env로 설정

--- src/main/java/com/sparta/WeatherWear/WeatherWearApplication.java
+++ src/main/java/com/sparta/WeatherWear/WeatherWearApplication.java
@@ -1,20 +1,14 @@
 package com.sparta.WeatherWear;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sparta.WeatherWear") // 필요한 패키지 경로 지정
 public class WeatherWearApplication {
 
 	@PostConstruct
