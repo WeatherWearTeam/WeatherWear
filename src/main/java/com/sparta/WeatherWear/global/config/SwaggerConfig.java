a28d8009b7a98fabbe617c01e66de6ae62f6ca45
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-10-03 10:12:44 +0900

    Swagger 이름 수정 + CORS 설정 추가

--- src/main/java/com/sparta/WeatherWear/global/config/SwaggerConfig.java
+++ src/main/java/com/sparta/WeatherWear/global/config/SwaggerConfig.java
@@ -18,8 +18,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("CodeArena Swagger")
-                .description("CodeArena 유저 및 인증 , ps, 알림에 관한 REST API")
+                .title("WeatherWear Swagger")
+                .description("WeatherWear Web Project API")
                 .version("1.0.0");
     }
 }
